88160e9a57074775f2a745b2525f807528252e44
Sena Nur <swnanurtr@gmail.com>
2024-01-07 00:47:27 +0300

    name eklendi

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,12 @@
 public class Player {
+    private String name;
     public boolean whiteSide;
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
     Player(boolean whiteSide){
         this.whiteSide = whiteSide;
     }
